6010b946db178f976ee12269fb6654d8d6291af0
Aryan2727-debug <78311368+Aryan2727-debug@users.noreply.github.com>
2025-11-15 17:08:24 +0530

    Updated Resume Nov. 2025

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typing" , {
-    strings : ["Web Designer", "Web Developer", "Programmer", "Coder"],
+    strings : ["Frontend Developer", "Software Developer", "Programmer", "Software Engineer"],
     typeSpeed : 100,
     BackSpeed : 60,
     loop:true
